5d7b61394b64260eba936b20767ba7273aa9e473
Maboroshi0327 <maboroshi0327@gmail.com>
2024-12-08 00:42:56 +0800

    Update test.py

--- ReCoNet-PyTorch/test.py
+++ ReCoNet-PyTorch/test.py
@@ -1,14 +1,21 @@
 import os
 import torch
 from torch.utils.data import Dataset, DataLoader
-import numpy as np 
+import numpy as np
 from totaldata import *
 from skimage import io, transform
 
-data=ConsolidatedDataset(MPI_path="../MPI_Data/" ,FC_path="../FlyingChairs2/")
+data = ConsolidatedDataset(MPI_path="../datasets/MPI-Sintel-complete/", FC_path="../datasets/FlyingChairs2/")
 print(len(data))
-img1, img2, mask, flow= data[230]
+img1, img2, mask, flow = data[230]
+
+# Check the data type and shape of the images
 print(img1.size(), img2.size(), mask.size(), flow.size())
-io.imsave("img1.png", img1.squeeze().permute(1,2,0).cpu().numpy())
-io.imsave("img2.png", img2.squeeze().permute(1,2,0).cpu().numpy())
-# io.imsave("mask.png", mask.squeeze().permute(1,2,0).cpu().numpy())
+print(img1.dtype, img2.dtype)
+
+# Convert th images to numpy arrays
+img1 = (img1.squeeze().permute(1, 2, 0).cpu().numpy() * 255).astype(np.uint8)
+img2 = (img2.squeeze().permute(1, 2, 0).cpu().numpy() * 255).astype(np.uint8)
+
+io.imsave("img1.png", img1)
+io.imsave("img2.png", img2)
